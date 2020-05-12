f38d16c15c3a40fb32829a8fb71b9aab53105ee7
maxma241 <marfi241@gmail.com>
2020-05-13 00:33:36 +0800

    feat: API add no-fail env

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const data = require('./mock-data.json');
 const { PORT = 3000 } = process.env;
+const noFailData = process.env.NO_FAIL === 'true'
 
 const random = (min, max) => Math.floor(Math.random() * (max - min) ) + min;
 const genAdId = () => `${+new Date()}-${random(0, 1000)}`;
@@ -10,7 +11,7 @@ const genAdId = () => `${+new Date()}-${random(0, 1000)}`;
 //get random ad from mock data
 const getAd = (type = '') => {
   const ads = type
-    ? data.filter(ad => ad.type === type || !ad.success)
+    ? data.filter(ad => noFailData ? ad.type === type : (ad.type === type || !ad.success))
     : data;
 
   const ad = ads[random(0, ads.length)];
